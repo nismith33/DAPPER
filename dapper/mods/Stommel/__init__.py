fa24280c4d75ea23d82bcb4519d0b621d8911d5d
Ivo Pasmans <i.c.pasmans@reading.ac.uk>
2022-09-12 13:21:34 +0100

    Use visitor pattern for fluxes.

--- dapper/mods/Stommel/__init__.py
+++ dapper/mods/Stommel/__init__.py
@@ -5,18 +5,21 @@ Module containing model equations for dimensionless Stommel model.
 import numpy as np
 from scipy.optimize import fsolve
 from dapper.mods.integration import rk4
+import dapper.mods as modelling
 import matplotlib.pyplot as plt
 import dataclasses
+from abc import ABC, abstractmethod
 
 #Directory to store figures. 
 fig_dir = "/home/ivo/dpr_data/stommel"
 
-mm2m = 1e-6 #convert millimeter to meter
+mm2m = 1e-3 #convert millimeter to meter
 year = 86400 * 365 #convert year to seconds
 func_type = type(lambda:0.0)
 
-#Convert observation functional into matrix operator. 
 def sample2linear(model):
+    """Convert observation functional into matrix operator."""
+    
     def linear(x, t):
         H = []
         for no in range(np.size(x)):
@@ -24,6 +27,7 @@ def sample2linear(model):
             x0[no] = 1.0
             H.append(model(x0,t))
         return np.array(H).T
+    
     return linear
     
 
@@ -58,19 +62,29 @@ Q_overturning = 18e6 #m3s-1
 mixing_depth = 50.0 #m
 #Estimated overturning
 gamma_ref = Q_overturning * LinearEquationState().rho_ref / (1027.5-1026.5) / (4.8e6 * 3.65e3) #m6/kg   
+#Ice volume greenland ice sheet 
+V_ice = 1710000 * 2 * 1e9 #m3
+
 
 @dataclasses.dataclass 
 class State:
+    """Class containing all attributes that make up a physical state."""
+    
+    #temperature in ocean basin
     temp: np.ndarray = np.array([[ 7.0, 17.0]]) #C 6,18
+    #salinity in ocean basis
     salt: np.ndarray = np.array([[35.0, 36.1]]) #ppt 35,36.5
-    
+    #surface salinity flux coefficient
     salt_diff: float = 3e-5 / mixing_depth / mm2m #m2/s/m = 1e3 mm/s
+    #surface heat flux coefficient
     temp_diff: float = 1e-4 / mixing_depth / mm2m #m2/s/m
+    #advective transport flux ceofficient 
     gamma: float = 0.0
-    
+    #time associated with state
     time: float = 0.0
     
     def to_vector(self):
+        """Convert state into a 1D array."""
         v = np.array([], dtype=float)
         for key, value in dataclasses.asdict(self).items():
             if key!='time':
@@ -78,17 +92,23 @@ class State:
         return v
     
     def from_vector(self, v):
+        """Read data from 1D into State object."""
         lb = 0
         for key, value in dataclasses.asdict(self).items():
             if key!='time':
                 ub = lb + max(1,np.size(value))
                 setattr(self, key, np.reshape(v[lb:ub], np.shape(value)))
                 lb = ub
+                
+        return self
             
     def zero(self):
+        """Set all values in State object to zero."""
         for key, value in dataclasses.asdict(self).items():
             if key!='time':
                 setattr(self, key, value * 0.0)
+                
+        return self
           
     @property
     def regime(self):
@@ -99,8 +119,14 @@ class State:
             return 'TH' #thermohaline circulation as in present
         else:
             return 'SA' #contra solution
+        
+    @property 
+    def N(self):
+        """Return size of state vector."""
+        return np.size(self.to_vector())
                 
 def array2states(array):
+    """Convert array with data to array of State objects."""
     shape = np.shape(array)
     array = np.reshape(array,(-1,shape[-1]))
     
@@ -113,10 +139,101 @@ def array2states(array):
     return states
 
 def states2array(states):
+    """Convert array of State objects into an array."""
     return np.array([s.to_vector() for s in states], dtype=float)
+
+class Flux:
+    """Class representing fluxes in model."""
+    
+    def __init__(self):
+        self.ens_member = 0
+    
+    def left(self, state):
+        """Return flux flowing into cell from left."""
+        return State().zero()
+    
+    def right(self, state):
+        """Return flux exiting cell to right."""
+        return State().zero()
+    
+    def top(self, state):
+        """Return flux exiting cell via top."""
+        return State().zero()
+    
+    def bottom(self, state):
+        """Return flux entering cell from bottom."""
+        return State().zero()
+        
+class AdvectiveFlux(Flux):
+    """ Class representing advective transport flux between pole and equator."""
+    
+    def __init__(self, eos):
+        """Set equation-of-state."""
+        self.eos = eos
+    
+    def transport(self, state):
+        #Density 
+        rho = self.eos(state.temp, state.salt)
+        #Transport volume
+        return np.abs(state.gamma * np.diff(rho, axis=1) / self.eos.rho_ref)
+    
+    def left(self, state):
+        flux = State().zero()
+        trans = self.transport(state)
+        flux.temp[:,1:] += trans * (state.temp[:,:-1] - state.temp[:,1:])
+        flux.salt[:,1:] += trans * (state.salt[:,:-1] - state.salt[:,1:])
+        return flux
+    
+    def right(self, state):
+        flux = State().zero()
+        trans = self.transport(state)
+        flux.temp[:,:-1] += trans * (state.temp[:,:-1] - state.temp[:,1:])
+        flux.salt[:,:-1] += trans * (state.salt[:,:-1] - state.salt[:,1:])
+        return flux
+    
+class TempAirFlux(Flux):
+    """Class representing heat flux through top of ocean."""
+    
+    def __init__(self, functions):
+        super().__init__()
+        self.functions = functions 
+        
+    def top(self, state):
+        n = np.mod(self.ens_member, len(self.functions))
+        flux = State().zero()
+        flux.temp[0] -= state.temp_diff * (self.functions[n](state.time) - state.temp[0]) * mm2m
+        return flux
+        
+class SaltAirFlux(Flux):
+    """Class representing salinity flux through top of ocean."""
+    
+    def __init__(self, functions):
+        super().__init__()
+        self.functions = functions 
+        
+    def top(self, state):
+        n = np.mod(self.ens_member, len(self.functions))
+        flux = State().zero()
+        flux.salt[0] -= state.salt_diff * (self.functions[n](state.time) - state.salt[0]) * mm2m
+        return flux
+        
+class EPFlux(Flux):
+    """Class representing evaporation/percipitation flux top of ocean."""
+    
+    def __init__(self, functions=np.array([lambda t: np.array([0.0, 0.0])], dtype=func_type) ):
+        super().__init__()
+        self.functions = functions 
+        
+    def top(self, state):
+        n = np.mod(self.ens_member, len(self.functions))
+        flux = State().zero()
+        flux.salt[0] -= state.salt[0] * self.functions[n](state.time)   
+        return flux
         
 @dataclasses.dataclass
 class StommelModel:
+    """Class containing all attributes and methods to represent the Stommel model."""
+    
     #Geometry of basin 
     dz: np.ndarray = np.array([[3.65e3, 3.65e3]]) #m depth
     dy: np.ndarray = np.array([[5.2e6, 5.2e6]]) #m latitude
@@ -129,26 +246,35 @@ class StommelModel:
     init_state: State = State()
     #Memory for model state
     state: State = State()
-    #Memory for tendency
-    tendency: State = State()
-    
-    #Atmospheric heat forcing
-    temp_air: np.array = np.array([lambda t: np.array([8.5, 26.0 ])], dtype=func_type) #C
-    #Atmospheric haline forcing
-    salt_air: np.array = np.array([lambda t: np.array([32.8, 36.6])], dtype=func_type) #ppt
-    #Evaporation/percipitation flux (evaportion is positive)
-    ep_flux: np.array = np.array([lambda t: np.array([0.0, 0.0])], dtype=func_type) 
     #Equation of state
     eos: LinearEquationState = LinearEquationState()
     #Ensemble member 
-    ens_member = 0
+    _ens_member = 0
+    #Fluxes 
+    fluxes = []
     
+    @property 
+    def ens_member(self):
+        """Get index currently active ensemble member."""
+        return self._ens_member 
+    
+    @ens_member.setter 
+    def ens_member(self, member):
+        """Set index currently active ensemble member."""
+        self._ens_member = member 
+        for flux in self.fluxes:
+            flux.ens_member = member 
     
     def __post_init__(self):
+        """Part of object initialization to be carried out after __init__ provided by dataclass."""
         self.init_state.gamma = self.default_gamma(self.init_state)
         self.state.gamma = self.default_gamma(self.state)
+        self.fluxes = self.default_fluxes()
         
     def default_gamma(self, state, Q=Q_overturning):
+        """Reverse engineer advective flux coefficient gamma using temperature/salinity fields in state
+        and meriodional overturning discharge Q."""
+        
         rho  =  self.eos(state.temp, state.salt)
         drho =  np.diff(np.sum(self.V * rho, axis=0)/np.sum(self.V, axis=0))[0]
         rho0 = self.eos.rho_ref
@@ -157,20 +283,23 @@ class StommelModel:
         
         return -Q * (rho0 / drho) / area 
     
-    def obs_ocean(self):
-        """ Observe ocean temperature and salinity. """
+    def default_fluxes(self):
+        """Set default fluxes."""
+        return [AdvectiveFlux(self.eos)]
+    
+    def obs_ocean(self, sig_temp=0.5, sig_salt=0.05):
+        """ Sampling operator for ocean temperature and salinity. """
+        
+        #Size of observations.
         M = 2*np.size(self.dz, 1)
         
+        #Function for taking a observation from single state. 
         def obs_TS1(x, t):
             self.state.time = t
-            year = 365*86400
+            self.state.from_vector(x)
+            return np.append(self.state.temp[0], self.state.salt[0])
         
-            if t<=30*year:
-                self.state.from_vector(x)
-                return np.append(self.state.temp, self.state.salt)
-            else:
-                return np.empty((4,))
-        
+        #Function for taking observation from ensemble of states. 
         def obs_model(x, t):            
             if np.ndim(x)==1:
                 return obs_TS1(x, t)
@@ -179,44 +308,39 @@ class StommelModel:
             else:
                 msg = "x must be 1D or 2D array."
                 raise TypeError(msg)
+          
+        #Observation error variance  
+        R = np.append(np.ones_like(self.state.temp[0]) * sig_temp**2,
+                      np.ones_like(self.state.salt[0]) * sig_salt**2) # C2,C2,ppt2,ppt2
         
-        Obs = {'M':M, 'model': obs_model, 'linear': sample2linear(obs_model)}
+        #DAPPER Observation operator
+        Obs = {'M':M, 'model': obs_model, 'linear': sample2linear(obs_model),
+               'noise': modelling.GaussRV(C=R, mu=np.zeros_like(R))}
+        
         return Obs
     
-    #Tendency from atmospheric forcing
-    def tendency_surface(self, tendency, state):
-        """Contribution surface fluxes to tendency."""
-        n = np.mod(self.ens_member+1, len(self.temp_air)) #member 0 is the truth
-        temp_air = self.temp_air[n](state.time)
-        tendency.temp[0] += (temp_air-state.temp[0]) * state.temp_diff * mm2m / self.dz[0] 
+    def tendency(self, state):
+        """Calculate tendency (d/dt) for the model."""
         
-        n = np.mod(self.ens_member+1, len(self.salt_air))
-        salt_air = self.salt_air[n](state.time)
-        tendency.salt[0] += (salt_air-state.salt[0]) * state.salt_diff * mm2m / self.dz[0] 
+        #Empty tendency
+        tendency = State().zero()
         
-        
-        n = np.mod(self.ens_member+1, len(self.ep_flux))
-        ep_flux = self.ep_flux[n](state.time)
-        tendency.salt[0] += ep_flux * state.salt[0] / self.dz[0]
-        
-        if state.time==86400:
-            print('rate ', ep_flux)
-        
-        return tendency
+        #Convergence vertical fluxes
+        for flux in self.fluxes:
+            top, bottom = flux.top(state), flux.bottom(state)
+            tendency.temp -= (top.temp - bottom.temp) / self.dz 
+            tendency.salt -= (top.salt - bottom.salt) / self.dz
     
-    #Tendency from advective transport
-    def tendency_trans(self, tendency, state):
-        """Contribution advective transport to tendency."""
-        A = 0.25 * (self.dz[:,1:] + self.dz[:,:-1]) * (self.dx[:,1:] + self.dx[:,:-1])
-    
-        rho = self.eos(state.temp, state.salt)
-        trans = np.abs(state.gamma * A * np.diff(rho, axis=1) / self.eos.rho_ref)
-    
-        tendency.temp[:, 1:] += trans * (state.temp[:,:-1]-state.temp[:,1:]) / self.V[:, 1:]
-        tendency.temp[:,:-1] -= trans * (state.temp[:,:-1]-state.temp[:,1:]) / self.V[:,:-1]
-    
-        tendency.salt[:, 1:] += trans * (state.salt[:,:-1]-state.salt[:,1:]) / self.V[:, 1:]
-        tendency.salt[:,:-1] -= trans * (state.salt[:,:-1]-state.salt[:,1:]) / self.V[:,:-1]
+        #Cross-section cells 
+        Aleft = self.dz[:,:-1] * self.dx[:,:-1] 
+        Aright = self.dz[:,1:] * self.dx[:,1:] 
+        
+        #Convergence horizontal fluxes. 
+        for flux in self.fluxes:
+            left, right = flux.left(state), flux.right(state)
+            tendency.temp -= (right.temp * Aright - left.temp * Aleft ) / self.V  
+            tendency.salt -= (right.salt * Aright - left.salt * Aleft ) / self.V 
+            
         return tendency
     
     #Total tendency 
@@ -225,11 +349,7 @@ class StommelModel:
         self.state.time = t
         self.state.from_vector(x)
         
-        self.tendency.zero()
-        self.tendency = self.tendency_surface(self.tendency, self.state)
-        self.tendency = self.tendency_trans(self.tendency, self.state)
-        
-        return self.tendency.to_vector()
+        return self.tendency(self.state).to_vector()
     
     #Forward model step for 1 ensemble member
     def step1(self, x0, t, dt):
@@ -241,9 +361,12 @@ class StommelModel:
         """ Step all model states forward. """
         if np.ndim(x)==1:
             x = self.step1(x, t, dt)
+        elif np.ndim(x)==2 and np.size(x,0)==1:
+            self.ens_member = 0
+            x[0] = self.step1(x[0], t, dt)
         elif np.ndim(x)==2:
             for no, x1 in enumerate(x):
-                self.ens_member = no
+                self.ens_member = no+1
                 x[no] = self.step1(x1, t, dt)
         else:
             msg = "x must be 1D or 2D array."
@@ -257,12 +380,22 @@ class StommelModel:
     
     def eta1(self, state):
         """Non-dimensional parameter eta1. See Dijkstra (2008)."""
-        dtemp = np.diff(self.temp_air[self.ens_member](state.time))[0]
+        temp_air = state.temp[0] * 0.0
+        for flux in self.fluxes:
+            if isinstance(flux, TempAirFlux):
+                temp_air = flux.functions[flux.ens_member](state.time)
+        
+        dtemp = np.diff(temp_air)[0]
         return dtemp / self.temp_scale(state)
     
     def eta2(self, state):
         """Non-dimensional parameter eta2. See Dijkstra (2008)."""
-        dsalt = np.diff(self.salt_air[self.ens_member](state.time))[0]
+        salt_air = state.salt[0] * 0.0
+        for flux in self.fluxes:
+            if isinstance(flux, SaltAirFlux):
+                salt_air = flux.functions[flux.ens_member](state.time)
+        
+        dsalt = np.diff(salt_air)[0]
         return (dsalt / self.salt_scale(state)) * self.eta3(state)
             
     def eta3(self, state):
@@ -272,16 +405,19 @@ class StommelModel:
         return R_S/ R_T
     
     def temp_scale(self, state):
+        """Factor to transform nondimensional to dimensional temperature."""
         A = np.mean(self.dx[0]) * np.mean(self.dz[0])
         gamma = state.gamma * A
         return self.trans_scale(state) / (gamma * self.eos.alpha_T)
     
     def salt_scale(self, state):
+        """Factor to transform nondimensional to dimensional salinity."""
         A = np.mean(self.dx[0]) * np.mean(self.dz[0])
         gamma = state.gamma * A
         return self.trans_scale(state) / (gamma * self.eos.alpha_S)  
         
     def trans_scale(self, state):
+        """Factor to transform nondimensional to dimensional advective transport."""
         R_T = np.mean(state.temp_diff * mm2m / self.dz[0])
         V = np.prod(self.V[0]) / np.sum(self.V[0])
         return R_T * V
@@ -297,7 +433,6 @@ class StommelModel:
         temp_eq  = self.eta1(state) / (1 +  np.abs(trans_eq))  
         return temp_eq * self.temp_scale(state)
         
-    
     def salt_eq(self, state, trans_eq):
         """Salinity difference equator_pole in equilibrium in ppt."""
         trans_eq = trans_eq / self.trans_scale(state)
@@ -309,21 +444,63 @@ class StommelModel:
         from scipy.optimize import root_scalar as find_roots
         import matplotlib.pyplot as plt
         
+        #Function of transport that is equal to zero in equilibrium.
         f = lambda q: (q - self.eta1(state) / (1+np.abs(q)) + 
                        self.eta2(state) / (self.eta3(state)+np.abs(q)))
         
+        #Evaluate values on grid. 
         q  = np.linspace(-3,3,30000)     
         fq = f(q)
-        
         q=.5*q[1:]+.5*q[:-1]
         fq=fq[1:]*fq[:-1]
         
+        #Find roots
         roots = [q1 for q1,f1 in zip(q,fq) if f1<0.]
         
-        return np.array(roots) * self.trans_scale(state)
+        return np.array(roots) * self.trans_scale(state) 
     
     
+def default_air_temp(N):
+    """ Unperturbed air temperature. """
+    return np.array([lambda t: np.array([8.5, 26.0 ]) for _ in range(N+1)], dtype=func_type)
+    
+def default_air_salt(N):
+    """ Unperturbed air salinity."""
+    return np.array([lambda t: np.array([32.8, 36.6]) for _ in range(N+1)], dtype=func_type)
+
+def default_air_ep(N, ep=np.array([0.,0.])):
+    """ Unperturbed air salinity."""
+    return np.array([lambda t: ep for _ in range(N+1)], dtype=func_type)
+
+def merge_functions(T, func1, func2):
+    """ Merge two functions with different time domains into 1."""
+    
+    def merged_func(t):
+        if t<=T:
+            return func1(t)
+        else:
+            return func2(t)
         
+    return merged_func
+
+def add_noise(func, seed, sig):
+    """ Add white noise to a forcing function."""
+    
+    def noised_func(t):
+        np.random.seed(seed + int(t/86400))
+        perturbation = np.random.normal(size=np.shape(sig)) * sig
+        return func(t) + perturbation
+        
+    return noised_func    
+
+def add_functions(func, func_add):
+    """ Add two functions. """
+    
+    def trend_func(t):
+        return func(t)+func_add(t)
+    
+    return trend_func
+
     
 # Display model information
 def display(model):
@@ -371,14 +548,6 @@ def add_warming(func, mu=np.array([3., 2.]), sigs=np.array([0., 0.])):
     def warmed_func(t):
         return func(t) + min(t, T) * rate 
     return warmed_func   
-
-def add_init_noise(func, seed, sig):
-    """ Add white noise to a forcing function."""
-    np.random.seed(seed)
-    perturbation = np.random.normal(size=np.shape(sig)) * sig
-    def noised_func(t):
-        return func(t) + perturbation
-    return noised_func    
 
 def time_figure(tseq):
     plt.close('all')
